5404b232420e0e9c1a19f0ded495e8991e9743de
Luis <santiagomorad@gmail.com>
2021-01-23 01:35:18 -0300

    fixing db schema for deployment

--- config/database.php
+++ config/database.php
@@ -74,7 +74,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'schema' => 'mutz_hub',
+            'schema' => env( "DB_SCHEMA" ),
             'sslmode' => 'prefer',
         ],
 
