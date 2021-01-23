f12371368463b16c5c297c528d651c274f0623a7
Luis <santiagomorad@gmail.com>
2021-01-23 02:29:32 -0300

    testing different deploy

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -33,6 +33,7 @@ class ShopController extends Controller
             on sho_id = men_shop
             group by m1.men_category,s1.sho_id,m1.cat_description"
         );
+        //p
         return response(
             ShopWithStatsResource::collection(
                 collect( $shops )->groupBy("sho_id")
